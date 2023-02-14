73510c9ebdf27d57be76ac6479317feebc075050
wk-ss <willianskeiichiss@gmail.com>
2023-02-14 13:48:15 -0300

    Atualizando

--- exerci/workshop2/ativ12.py
+++ exerci/workshop2/ativ12.py
@@ -29,11 +29,40 @@ if desicao==1:
     valort=valores[0]
     valortp=valores[1]
 
+def pipa(quartos,pessoas):
+    valorpessoa=pessoas*75
+    if quartos==2:
+        valorquartos=600
+    elif quartos==3:
+        valorquartos=900
+    valort=valorquartos+valorpessoa
+    valortp=valort/pessoas
+    return valort,valortp
+
+def fortaleza(quartos,pessoas):
+    valorpessoa=pessoas*60
+    if quartos==4:
+        valorquartos=1120
+    elif quartos==3:
+        valorquartos=950
+    valort=valorquartos+valorpessoa
+    valortp=valort/pessoas
+    return valort,valortp
+
+pessoas=int(input(f"Digite a quantidade de pessoas:"))
+desicao=int(input(f"Selecione a cidade escolhida: (1 - Pipa e 2 - Fortaleza)"))
+quartos=int(input(f"Digite a quantidade de quartos:"))
+if desicao==1:
+    valores= pipa(quartos,pessoas)
+
+    valort=valores[0]
+    valortp=valores[1]
+
     print("Digite a quantidade de pessoas:")
     print("Selecione a cidade escolhida: (1 - Pipa e 2 - Fortaleza)")
     print("Digite a quantidade de quartos:")
-    print(f"Valor total da viagem: R$ {float(valort)}")
-    print(f"Valor por pessoa: R$ {float(valortp)}")
+    print(f"Valor total da viagem: R$ %.2f" % valort)
+    print(f"Valor por pessoa: R$ R$ %.2f" % valortp)
 elif desicao==2:
     valores=fortaleza(quartos,pessoas)
     valort=valores[0]
@@ -41,5 +70,5 @@ elif desicao==2:
     print("Digite a quantidade de pessoas:")
     print("Selecione a cidade escolhida: (1 - Pipa e 2 - Fortaleza)")
     print("Digite a quantidade de quartos:")
-    print(f"Valor total da viagem: R$ {float(valort)}")
-    print(f"Valor por pessoa: R$ {float(valortp)}")
+    print(f"Valor total da viagem: R$ %.2f" % valort)
+    print(f"Valor por pessoa: R$ R$ %.2f" % valortp)
